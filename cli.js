8693a628a6af2548059ad6e7d3a690b90530054b
egoing <egoing@gmail.com>
2020-02-03 01:11:36 +0900

    프로젝트 관리 기능 추가

--- cli.js
+++ cli.js
@@ -1,22 +1,27 @@
 #! /usr/bin/env node
-
 var fs = require('fs');
 var exec = require('child_process').exec;
 var meow = require('meow');
 const os = require('os');
+const city = require('./city');
+const Conf = require('conf');
+const config = new Conf();
+var list = config.get('vscode-new-list');
 
 const cli = meow(`
 	Usage
-	  $ vscode-new [project name's prefix]
+      $ cod-ing new [project name] 
+      $ cod-ing list 
+      $ cod-ing run 
 
 	Options
       --no-code, -nc run without vscode
       --path, -p 
       --no-suffix, -ns
 	Examples
-      $ npx vscode-new 'react'  create directory to /tmp/react__2020-2-1--5-28-16 and launch vscode
-      $ npx vscode-new --no-code  create directory to /tmp/react__2020-2-1--5-28-16 and not launch vscode
-      $ npx vscode-new --path '~/dev/'  create directory to ~/dev/react__2020-2-1--5-28-16 and launch vscode 
+      $ npx vscode-new 'react'  create directory to /tmp/react__[city name] and launch vscode
+      $ npx vscode-new --no-code  create directory to /tmp/react__[city name] and not launch vscode
+      $ npx vscode-new --path '~/dev/'  create directory to ~/dev/react__[city name] and launch vscode 
 `, {
     flags: {
         'no-code': {
@@ -24,60 +29,106 @@ const cli = meow(`
         },
         path: {
             alias: 'p'
-        }, 
-        'no-suffix':{
+        },
+        'no-suffix': {
             alias: 'ns'
         }
     }
 });
 
-function resolveHome(filepath) {
-    var path = require('path');
-    if (filepath[0] === '~') {
-        return path.join(process.env.HOME, filepath.slice(1));
+function getDirName() {
+    function resolveHome(filepath) {
+        var path = require('path');
+        if (filepath[0] === '~') {
+            return path.join(process.env.HOME, filepath.slice(1));
+        }
+        return filepath;
     }
-    return filepath;
+
+    const tempDirectorySymbol = getTempDir();
+
+    var basePath = null;
+    if (cli.flags.path === undefined) {
+        basePath = global[tempDirectorySymbol];
+    } else {
+        basePath = resolveHome(cli.flags.path);
+    }
+
+    var fileName = [];
+    // ex cod-ing new test
+    if (cli.input[1] !== undefined) {
+        fileName = cli.input[1];
+    } else {
+        var m = new Date();
+        var fileName = city[Math.floor(Math.random() * city.length)];
+    }
+
+    return basePath + '/' + fileName;
+}
+function getTempDir() {
+    const tempDirectorySymbol = Symbol.for('__RESOLVED_TEMP_DIRECTORY__');
+    if (!global[tempDirectorySymbol]) {
+        Object.defineProperty(global, tempDirectorySymbol, {
+            value: fs.realpathSync(os.tmpdir())
+        });
+    }
+    return tempDirectorySymbol;
 }
 
-const tempDirectorySymbol = Symbol.for('__RESOLVED_TEMP_DIRECTORY__');
-if (!global[tempDirectorySymbol]) {
-    Object.defineProperty(global, tempDirectorySymbol, {
-        value: fs.realpathSync(os.tmpdir())
+function project_list() {
+    var list = config.get('vscode-dir-list');
+    var Table = require('cli-table3');
+    var table = new Table({
+        head: ['id', 'path']
+    });
+    for (var i = 0; i < list.length; i++) {
+        table.push([i, list[i].path]);
+    }
+    console.log(table.toString());
+}
+function project_run(path){
+    var commands = [
+        `cd ${path}`
+    ]
+    if (cli.flags.code === undefined) {
+        commands.push(`code ${path}`);
+    }
+    exec(commands.join('&&'), function callback(error, stdout, stderr) {
+        if (error) {
+            console.log('error : ' + error);
+        }
+        var list = config.get('vscode-dir-list');
+        list = list === undefined ? [] : list;
+        list = list.filter(function(e){
+            return e.path !== path;
+        })
+        list.unshift({
+            path: path,
+            run: 'vscode',
+            arr: []
+        });
+        config.set('vscode-dir-list', list);
+        console.log("Open temp project in " + path);
     });
 }
-
-
-
-var basePath = null;
-if(cli.flags.path === undefined){
-    basePath = global[tempDirectorySymbol];
-} else {
-    basePath = resolveHome(cli.flags.path);
-}
-
-var fileName = [];
-if(cli.input.length>0){
-    fileName = cli.input;
-}
-
-if (cli.flags.suffix === undefined) {
-    var m = new Date();
-    var suffix = m.getUTCFullYear() +"-"+ (m.getUTCMonth()+1) +"-"+ m.getUTCDate() + "--" + m.getUTCHours() + "-" + m.getUTCMinutes() + "-" + m.getUTCSeconds();
-    fileName.push(suffix);
-}
-
-var newDirName = basePath+'/'+fileName.join('_');
-
-fs.mkdirSync(newDirName, 0744);
-var commands = [
-    `cd ${newDirName}`
-]
-if (cli.flags.code === undefined) {
-    commands.push(`code ${newDirName}`);
-}
-exec(commands.join('&&'), function callback(error, stdout, stderr) {
-    if (error) {
-        console.log('error : ' + error);
+if (cli.input[0] === "list") {
+    project_list();
+} else if (cli.input[0] === "run") {
+    project_list();
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question("Select the id number you want to run. ", function(id) {
+        var list = config.get('vscode-dir-list');
+        project_run(list[Number(id)].path);
+        rl.close();
+    });
+} else if(cli.input[0] === undefined || cli.input[0] === "new" || cli.input[0] === "create" || cli.input[0] === "make"){
+    var newDirName = getDirName();
+    if(!fs.existsSync(newDirName)){
+        fs.mkdirSync(newDirName);
     }
-    console.log("Created temp project in " + newDirName);
-});
+    project_run(newDirName);
+}
